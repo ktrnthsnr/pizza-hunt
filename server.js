6fc402c99d68fc199e731930a0cc2b4709ae5df2
ktrnthsnr <asakthisner@hotmail.com>
2020-07-11 02:48:20 -0700

    mongoose lib connect to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
@@ -8,5 +8,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
+
+// mongoose library connection to local mongo database
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost Port: ${PORT}`));
