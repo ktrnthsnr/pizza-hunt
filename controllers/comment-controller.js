7779f77e214a8416ad768c491eabb02fcdea9b04
ktrnthsnr <asakthisner@hotmail.com>
2020-07-11 17:55:28 -0700

    added comment controller to add\del comments

---
+++ controllers/comment-controller.js
@@ -0,0 +1,53 @@
+//import models needed
+const { Comment, Pizza } = require('../models');
+
+// create object
+
+const commentController = {
+    // add comment to pizza, return a pizza Promise
+    addComment({ params, body }, res) {
+        console.log(body);
+        Comment.create(body)
+          .then(({ _id }) => {
+            return Pizza.findOneAndUpdate(
+              { _id: params.pizzaId },
+              // -- update the array with $push method, $ sign denotes MongoDB-based function
+              { $push: { comments: _id } }, // -- push method will add comments' ID to the pizza
+              { new: true }
+            );
+          })
+          .then(dbPizzaData => {
+            if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
+            }
+            res.json(dbPizzaData);
+          })
+          .catch(err => res.json(err));
+      },
+  
+    // remove comment, remove comment & remove comment ID from its pizza 
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete({ _id: params.commentId })
+          .then(deletedComment => {
+            if (!deletedComment) {
+              return res.status(404).json({ message: 'No comment with this id!' });
+            }
+            return Pizza.findOneAndUpdate(
+              { _id: params.pizzaId },
+              { $pull: { comments: params.commentId } },
+              { new: true }
+            );
+          })
+          .then(dbPizzaData => {
+            if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
+            }
+            res.json(dbPizzaData);
+          })
+          .catch(err => res.json(err));
+      }
+  };
+  
+  module.exports = commentController;
