f3c470d9041b8fc0d94d630cc4ede33cfd2ca6b6
ktrnthsnr <asakthisner@hotmail.com>
2020-07-17 00:02:11 -0700

    establish connection to indexdb

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -2,5 +2,34 @@
 
 // variable will store the db object when connected
 let db;
-// request variable as the event listener, will connect to the IndexedDB db 'pizza_hunt', an dset to version 1
+// request variable as the event listener, will connect to the IndexedDB db 'pizza_hunt', to version 1
 const request = indexedDB.open('pizza_hunt', 1);
+
+// event listeners
+
+// event emits if db ver changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+    // ref to the db
+    const db = event.target.result;
+    // creates object store (or table) named `new_pizza`, set to  auto-increment on primary key
+    db.createObjectStore('new_pizza', { autoIncrement: true });
+  };
+
+  // if successful 
+request.onsuccess = function(event) {
+    // when db is created with its obj store (from onupgradedneeded event above)
+        // or established a connection, save ref to database in global var
+    db = event.target.result;
+  
+    // check if app is online, 
+        //if yes, run uploadPizza() to send all local database data to the API
+    if (navigator.onLine) {
+      // placeholder
+      // uploadPizza();
+    }
+  };
+  
+  request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+  };
